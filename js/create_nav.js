6bf571fc6123f69fb47258d8c92d40b41a7b91c1
giovannidiana <g.diana.mail@gmail.com>
2021-02-25 14:00:52 +0100

    modified navbar

--- js/create_nav.js
+++ js/create_nav.js
@@ -1,27 +1,21 @@
 function create_nav(title){
-    var div_outer=d3.select("body").append("div")
-                                   .style("display","flex")
-                                   .style("flex-wrap","wrap-reverse")
-                                   .style("justify-content", "space-between");
 
-    var div_header = div_outer.append("div").style("float","left")
-
-    div_header.append("h2")
-        .style("margin",0)
-        .style("padding","8px")
+    d3.select("body").append("h2")
         .style("white-space","nowrap")
+        .style("margin-top","60px")
+        .style("margin-bottom","0px")
         .text(title)
 
-    var div_nav=div_outer.append("div")
-
-    var nav=div_nav.append("ul").attr("class","ulnav")
-    nav.append("li").attr("class","nav").append("a")
+    var div_nav=d3.select("body").append("div").attr("class","navbar")
+    div_nav.append("a")
         .attr("href","index.html")
         .text("Index")
-    nav.append("li").attr("class","nav").append("a")
+    div_nav.append("a")
         .attr("href","publications.html")
         .text("Publications")
-    nav.append("li").attr("class","nav").append("a")
+    div_nav.append("a")
         .attr("href","softwares.html")
         .text("Software")
+
+
 }
