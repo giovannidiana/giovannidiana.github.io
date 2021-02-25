947bcc5e445e973dcdaf31f2f3ce079cce9a76d3
giovannidiana <g.diana.mail@gmail.com>
2021-02-25 11:34:07 +0100

    fixed a few things and added responsiveness to resize

--- js/funny_notimer.js
+++ js/funny_notimer.js
@@ -1,16 +1,15 @@
-var svg_ph = d3.select("#svg1")
 var maxheigth=10
 
-function gentrace(){
+function gentrace(n){
     var vector = [];
     var c=0
     var x=0
     var i=0
-    while(i<1000){
+    while(i<n){
         i+=1
         var s = false
         var c1 = c*math.exp(-2)
-        if(math.random()<0.5 && i>400) s=true
+        if(math.random()<0.5 && i>n/2) s=true
         if(s) c1+=2*math.abs(randnScalar())
         vector.push({x1:i-1,x2:i,y1:c,y2:c1})
         c=c1
@@ -18,25 +17,31 @@ function gentrace(){
     return(vector)
 }
 
-var trace = gentrace();
+function display_trace(){
+    //console.log("display trace")
+    var N=math.round(d3.select("body").node().getBoundingClientRect().width);
+    d3.select("#svg1").attr("viewBox",[0,0,N,10].join(" "))
+    tr = gentrace(N)
 
-svg_ph.selectAll("line")
-    .data(trace)
-    .enter()
-    .append("line")
-    .attr("x1",function(d){ return d.x1})
-    .attr("x2",function(d){ return d.x2})
-    .attr("y1",function(d){return maxheigth-d.y1})
-    .attr("y2",function(d){ return maxheigth-d.y2})
-    .attr("stroke-width",0.5)
-    .attr("stroke","black")
-    .style("opacity", 0)
-  .transition()
-  .duration(0)
-  .delay(function(_, i) {
-    return i})
-  .style("opacity",1)
+    d3.select("#svg1").selectAll("line").remove()
 
+    d3.select("#svg1").selectAll("line")
+        .data(tr)
+        .enter()
+        .append("line")
+        .attr("x1",function(d){ return d.x1})
+        .attr("x2",function(d){ return d.x2})
+        .attr("y1",function(d){return maxheigth-d.y1})
+        .attr("y2",function(d){ return maxheigth-d.y2})
+        .attr("stroke-width",0.5)
+        .attr("stroke","black")
+        .style("opacity", 0)
+        .transition()
+        .duration(0)
+        .delay(function(_, i) {
+            return i})
+        .style("opacity",1)
+}
 
 // Gaussian random number (mean = 0, variance = 1;
 //  Gaussian noise with the polar form of the Box-Muller transformation
